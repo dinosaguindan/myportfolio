e14979178d342960ceb08e743986af67f2830b45
Gino Fernando V. Nones <gfvnones@LPTP2018IT00125.sscgi.com.inc>
2020-09-01 15:20:54 +0800

    Updates

---
+++ precache-manifest.45c776ae91c771d123de7482bb846bc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12282ade909b500e10099767a2993037",
+    "url": "/mern-simple-app/index.html"
+  },
+  {
+    "revision": "c9b6d34081838cc847dc",
+    "url": "/mern-simple-app/static/css/2.6cb6ad63.chunk.css"
+  },
+  {
+    "revision": "459738dd496225785fa6",
+    "url": "/mern-simple-app/static/css/main.b60ec321.chunk.css"
+  },
+  {
+    "revision": "c9b6d34081838cc847dc",
+    "url": "/mern-simple-app/static/js/2.df8d0e4e.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/mern-simple-app/static/js/2.df8d0e4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "459738dd496225785fa6",
+    "url": "/mern-simple-app/static/js/main.4b10a9f7.chunk.js"
+  },
+  {
+    "revision": "5021095f92956e5efbf3",
+    "url": "/mern-simple-app/static/js/runtime-main.e8a255b3.js"
+  }
+]);
